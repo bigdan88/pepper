191ca4ebf214b2e186db51b9dea5ac3bda148cdd
bigdan88 <daniel88han@gmail.com>
2023-03-07 21:21:57 -0500

    trying to fix

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/entry/_layout.svelte.0a2cf4e3.js';
-export const imports = ["_app/immutable/entry/_layout.svelte.0a2cf4e3.js","_app/immutable/chunks/index.2ee51f7d.js"];
-export const stylesheets = ["_app/immutable/assets/_layout.07d81a05.css"];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.ea1b1598.js';
-export const imports = ["_app/immutable/entry/error.svelte.ea1b1598.js","_app/immutable/chunks/index.2ee51f7d.js","_app/immutable/chunks/singletons.df0f1e6a.js"];
-export const stylesheets = [];
-export const fonts = [];
